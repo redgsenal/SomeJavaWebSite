dff89e68910b707a29592197237441995f56998a
DDB\reggie.senal <reggie.senal@sg.tribalworldwide.com>
2016-12-12 13:41:14 +0800

    add new sample page

---
+++ WebContent/views/hello/hello.jsp
@@ -0,0 +1,1 @@
+<p>Just saying Hello!!!</p>
---
+++ src/com/servlet/hello/HelloServlet.java
@@ -0,0 +1,48 @@
+package com.servlet.hello;
+
+import com.servlet.router.GenericServlet;
+import java.io.IOException;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends GenericServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloServlet() {
+    	super("views/hello/hello.jsp");
+    }
+       
+    /**
+     * @see GenericServlet#GenericServlet(String)
+     */
+    public HelloServlet(String content) {
+        super(content);
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		super.doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
