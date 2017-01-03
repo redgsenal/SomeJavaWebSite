cf865b383324537bb6e1a841d848f731d43058ed
DDB\reggie.senal <reggie.senal@sg.tribalworldwide.com>
2017-01-03 16:57:48 +0800

    updates clean up

--- src/com/servlet/router/verify/VerifyUserServlet.java
+++ src/com/servlet/router/verify/VerifyUserServlet.java
@@ -5,7 +5,6 @@ import com.site.utils.NonceList;
 
 import java.io.IOException;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
